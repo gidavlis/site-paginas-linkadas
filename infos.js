befea75704393861f5bc2bac2839ae6aa75b3fb2
Gid <gagideias@gmail.com>
2025-09-03 11:48:10 -0300

    primeiro commit - paginas interativas

---
+++ infos.js
@@ -0,0 +1,17 @@
+var nomeCompleto = "Gideias Alves";
+var anoCorrente = "2025";
+var twitter = "@exerciciogid";
+var facebook = "@exerciciogid";
+var instagram = "@exerciciogid";
+var linkedin = "@exerciciogid";
+var sexo = "Masculino";
+var localizacaoPolo = "Higienópolis - SP";
+
+document.write("<p>Nome Completo: " + nomeCompleto + "</p>");
+document.write("<p>Ano Corrente: " + anoCorrente + "</p>");
+document.write("<p>Twitter: " + twitter + "</p>");
+document.write("<p>Facebook: " + facebook + "</p>");
+document.write("<p>Instagram: " + instagram + "</p>");
+document.write("<p>LinkedIn: " + linkedin + "</p>");
+document.write("<p>Sexo: " + sexo + "</p>");
+document.write("<p>Localização do Polo: " + localizacaoPolo + "</p>");
